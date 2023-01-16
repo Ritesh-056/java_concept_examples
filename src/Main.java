0c9d1350bca02aa46feccbe5b11af7a33a639f2f
Ritesh-056 <baralriteshrtz@gmail.com>
2023-01-16 22:37:17 +0545

    dynamic push & pop

--- src/Main.java
+++ src/Main.java
@@ -2,26 +2,16 @@ public class Main {
     public static void main(String[] args) {
 
         StackImplement stackImplement = new StackImplement(3);
-        stackImplement.push(2);
-        stackImplement.push(3);
-        stackImplement.push(4);
-        stackImplement.push(5);
-        stackImplement.push(6);
+        int maxItr  = 5;
 
-        int result1 = stackImplement.pop();
-        int result2 = stackImplement.pop();
-        int result3 = stackImplement.pop();
-        int result4 = stackImplement.pop();
-        int result5 = stackImplement.pop();
+        //push in stack
+        for(int i=0 ; i <maxItr ; i++){
+            stackImplement.push(i*2);
+        }
 
-
-        System.out.println(result1);
-        System.out.println(result2);
-        System.out.println(result3);
-        System.out.println(result4);
-        System.out.println(result5);
-
-
-
+        //pop from stack
+        for(int i=0 ; i <maxItr ; i++){
+            System.out.println("Item popped : "+ stackImplement.pop());
+        }
     }
 }
